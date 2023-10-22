d64899fb5b734f8fbc8701c0f453e1e3af7c829a
EmmaTBxx <maneyras@gmail.com>
2023-10-22 12:49:03 -0700

    final

--- pall.c
+++ pall.c
@@ -5,12 +5,12 @@
  * @counter: no used
  * Return: no return
  */
-void f_pall(stack_t **head. unsigned int counter)
+void f_pall(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	(void)counter;
 
-	h = *head
+	h = *head;
 	if (h == NULL)
 		return;
 	while (h)
